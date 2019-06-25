cb3cce2318833d9be53b861070f8b6d20265faef
sourabhgupta811 <32221826+sourabhgupta811@users.noreply.github.com>
2019-06-25 06:21:26 +0530

    initial commit

--- network/src/main/java/com/sourabh/network/constants/NetworkConstants.kt
+++ network/src/main/java/com/sourabh/network/constants/NetworkConstants.kt
@@ -1,7 +1,7 @@
 package com.sourabh.network.constants
 
 object NetworkConstants{
-    val TMDB_API_KEY = "9b7f898dc2aaf753630ac6af5f63a500"
+    val TMDB_API_KEY = ""
     val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
     //Example Api Request
 //    https://api.themoviedb.org/3/movie/550?api_key=9b7f898dc2aaf753630ac6af5f63a500
